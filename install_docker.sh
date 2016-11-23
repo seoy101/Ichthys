aeb5cd0e7aba23ef910d15dcf1aebe23757c4e1c
seoy101 <fighton.yj@gmail.com>
2016-11-23 14:30:17 +0900

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install -y -q apt-transport-https ca-certificates 
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D 
-sudo apt-key adv -k 58118E89F3A912897C070ADBF76221572C52609D >/dev/null 
+apt-get install -y -q apt-transport-https ca-certificates && apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && apt-key adv -k 58118E89F3A912897C070ADBF76221572C52609D >/dev/null 
 
-sudo mkdir -p /etc/apt/sources.list.d
-sudo touch /etc/apt/sources.list.d/docker.list
-sudo echo deb \[arch=amd64\] https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get install -y -q docker-engine=1.11.2-0~trusty
-sudo apt-mark hold docker-engine
+mkdir -p /etc/apt/sources.list.d
+touch /etc/apt/sources.list.d/docker.list
+echo deb \[arch=amd64\] https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y -q docker-engine
+sapt-mark hold docker-engine
