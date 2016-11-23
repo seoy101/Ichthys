7a2a7cc15e5b3371a37d617532c12450486b14b5
seoy101 <fighton.yj@gmail.com>
2016-11-23 16:12:30 +0900

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,8 +2,8 @@
 
 apt-get install -y -q apt-transport-https ca-certificates && apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && apt-key adv -k 58118E89F3A912897C070ADBF76221572C52609D >/dev/null 
 
-mkdir -p /etc/apt/sources.list.d
-touch /etc/apt/sources.list.d/docker.list
+#mkdir -p /etc/apt/sources.list.d
+#touch /etc/apt/sources.list.d/docker.list
 # ubuntu 16.04(Xenial)
 #echo deb \[arch=amd64\] https://apt.dockerproject.org/repo ubuntu-xenial main > /etc/apt/sources.list.d/docker.list
 
@@ -14,7 +14,7 @@ touch /etc/apt/sources.list.d/docker.list
 #echo deb \[arch=amd64\] https://apt.dockerproject.org/repo ubuntu-vivid main > /etc/apt/sources.list.d/docker.list
 
 # ubuntu 14.04(trusty)
-echo deb \[arch=amd64\] https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list
+#echo deb \[arch=amd64\] https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list
 
 # ubuntu 13.10(Saucy)
 #echo deb \[arch=amd64\] https://apt.dockerproject.org/repo ubuntu-saucy main > /etc/apt/sources.list.d/docker.list
@@ -27,5 +27,5 @@ echo deb \[arch=amd64\] https://apt.dockerproject.org/repo ubuntu-trusty main > 
 apt-get update -y
 apt-cache policy docker-engine
 
-#apt-get install -y -q docker-engine
+#apt-get install -y -q docker-engine=1.11.2-0~trusty
 #apt-mark hold docker-engine
